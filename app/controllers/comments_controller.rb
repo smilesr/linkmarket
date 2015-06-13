ac885cd33a8192e8196dc820663045262483d107
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-13 18:00:57 -0400

    1st crack at Index View and changed Comments Controller variable from written at to created at

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
+
+
+
+class CommentsController < ApplicationController
 def new
   @link = Link.create(link_id: params[:id])
   @all_comments = @link.comments.all
   render :new 
-=======
-class CommentsController < ApplicationController
-  def new
   end
->>>>>>> 9c02ec09ab802ec898b3f26529b7ca20ff8787e9
 end
 
 def create
   @comment = Link.comments.create(content = params[:content],
                       user_id = user.id,
-                      written_at = DateTime.now)
+                      created_at = DateTime.now)
 
   redirect_to :root  
 end
