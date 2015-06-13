afb35ef3fa727a8675ded05d7d5344b8dfe2d1a1
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-12 22:39:29 -0400

    1st complete go at commments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 def new
- @all_comments = Comment.all
- render :new 
+  @link = Link.create(link_id: params[:id])
+  @all_comments = @link.comments.all
+  render :new 
 end
 
 def create
-  @comment = Link.comments.create(content = _____,
-                      user_id = _______,
-                      written_at = _______)
+  @comment = Link.comments.create(content = params[:content],
+                      user_id = user.id,
+                      written_at = DateTime.now)
 
   redirect_to :root  
 end
