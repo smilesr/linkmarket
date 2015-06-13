1ba7adb38addda2c10dc8804a231eef67ee20c60
Philip Crawford Wood <pwood1284@gmail.com>
2015-06-13 14:55:49 -0400

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
-def new
-  @link = Link.create(link_id: params[:id])
-  @all_comments = @link.comments.all
-  render :new 
+class CommentsController < ApplicationController
+  def new
+    @link = Link.create(link_id: params[:id])
+    @all_comments = @link.comments.all
+    render :new 
+  end
+  
+  def create
+    @comment = Link.comments.create(content = params[:content],
+                        user_id = user.id,
+                        created_at = DateTime.now)
+  
+    redirect_to :root  
+  end
 end
-
-def create
-  @comment = Link.comments.create(content = params[:content],
-                      user_id = user.id,
-                      created_at = DateTime.now)
-
-  redirect_to :root  
-end
-
