a2e8c464431b6157bb415ada95c381af0fd3a1ca
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-12 21:18:32 -0400

    basic skeleton of comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 def new
+ @all_comments = Comment.all
+ render :new 
 end
+
+def create
+  @comment = Link.comments.create(content = _____,
+                      user_id = _______,
+                      written_at = _______)
+
+  redirect_to :root  
+end
+
