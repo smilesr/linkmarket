e024747ae7938ca16471f9cb80c1c7b071ffe4cd
pwood1284@gmail.com <your_email@whatever.com>
2015-06-13 15:47:58 -0400

    copy and pasted comments controller code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
 class CommentsController < ApplicationController
+  class CommentsController < ApplicationController
+    def new
+      @link = Link.create(link_id: params[:id])
+      @all_comments = @link.comments.all
+      render :new
+    end
+
+    def create
+      @comment = Link.comments.create(content = params[:content],
+                          user_id = user.id,
+                          created_at = DateTime.now)
+
+      redirect_to :root
+    end
+   end
 
 end
