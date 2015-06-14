d8d2c01092506cce19151d1285c03c52dfd819bd
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-14 12:24:20 -0400

    1st draft of Links Controller, plus fixed some syntax issues on Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,18 @@
 class CommentsController < ApplicationController
 def new
-  if @user_id
+  if @user
     @link = Link.create(link_id: params[:id])
     @all_comments = @link.comments.all
     render :new
   else
-    redirect_to :users_login
+    redirect_to :users_login_path
   end
 end
 
 def create
-  @comment = @link.comments.create(content = params[:content],
-                      user_id = user.id,
-                      created_at = DateTime.now)
+  @comment = @link.comments.create(content: params[:content],
+                      user_id: user.id,
+                      created_at: DateTime.now)
   redirect_to :root  
 end
 
