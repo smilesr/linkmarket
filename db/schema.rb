95874e9483d241b05a44e1ca122a671359febed9
pwood1284@gmail.com <your_email@whatever.com>
2015-06-15 15:15:21 -0400

    added devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611212902) do
+ActiveRecord::Schema.define(version: 20150615191159) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 20150611212902) do
     t.string   "title"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
