41a04991a1d6a432da75a3906ee1e8b062612d67
pwood1284@gmail.com <your_email@whatever.com>
2015-06-15 15:03:46 -0400

    fixed bugs

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,19 +6,19 @@ class LinksController < ApplicationController
     render :index
   end
 
-  def new 
+  def new
 
-  if session
-    # @user = User.create(email: params[@user])
-    render :new
-  else
-    redirect_to users_login_path
-  end
+    if session
+      # @user = User.create(email: params[@user])
+      render :links
+    else
+      redirect_to users_login_path
+    end
 
   end
 
   def create
-    @link = @user.links.create(destination: params[:destination],
+    @link = Link.create(destination: params[:destination],
                               title: params[:title],
                               created_at: DateTime.now)
     redirect_to :root
